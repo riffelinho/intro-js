6453498b31ec649d992b11e9a72927159dcdc4ba
Riffelinho <riffelkemuel@gmail.com>
2022-02-02 20:30:27 -0300

    arreglo de la act6

--- actividad 6/ejes.js
+++ actividad 6/ejes.js
@@ -1,12 +1,22 @@
 1
 
-let newArray = [eagle, parrot, monkey, boar, lion]
+var animals = ["eagle", "parrot", "monkey", "boar", "lion"]
+var comingSoonAnimal = ["panter", "dragon", "turtle"]
 
-var comingSOONAnimals = [panter, dragon, turtle]
+var arrAnimal = animals.concat(comingSoonAnimal)
 
+console.log(arrAnimal)
 2 
-arreglo.sort((a, b) => a - b )
-3
- animals.push('cow')
-4
- animal.splice(0,1)
+
+var arr = [4,6,1,0,8,2]
+
+arr.sort()
+console.log(arr)
+3 
+
+animals.push("cow")
+console.log(animals)
+4 
+
+animals.splice(0,1)
+console.log(animals) 
